60e0cd3fba41bf00030835f83c188bde0e354239
EthelHsiao <ethelhsiao0616@gmail.com>
2025-03-19 16:20:33 +0800

    simplify

--- sum_of_digits.s
+++ sum_of_digits.s
@@ -1,65 +1,62 @@
-# Student ID: [Your Student ID here]
+#112550179
 
 .data
-prompt: .asciiz "Enter an integer: "
+input_msg: .asciiz "Enter an integer: "
 newline: .asciiz "\n"
 
 .text
 .globl main
 
-# sumOfDigits(int n)
+main:
+# print input_msg on the console interface
+    li      $v0, 4
+    la      $a0, input_msg
+    syscall
+    
+#read int
+    li      $v0, 5
+    syscall
+    move    $s0, $v0      
+    
+#Call sumOfDigits
+    move    $a0, $s0      
+    jal     sumOfDigits    
+    
+#print result
+    li      $v0, 1           
+    move    $a0, $v0    
+    syscall
+    
+#print newline
+    li      $v0, 4
+    la      $a0, newline
+    syscall
+    
+#exit program
+    li      $v0, 10
+    syscall
+
+#-----------implement sum of digits--------------
+
+#sumOfDigits(int n)
 sumOfDigits:
-    # Initialize sum to 0
-    li $v0, 0
+    #initilize sum=0
+    li      $v0, 0
     
 sum_loop:
-    # Check if n <= 0
-    ble $a0, $zero, sum_done
+#check if n <= 0
+    ble $a0, $zero, sum_done #a0<=zero, jump to sun_done
     
-    # Get last digit: n % 10
     li $t0, 10
-    div $a0, $t0
-    mfhi $t1            # $t1 = n % 10
-    
-    # Add digit to sum
+    div $a0, $t0 #a0/t0
+    mfhi    $t1 #mfhi take %   
     add $v0, $v0, $t1
-    
-    # Remove last digit: n /= 10
+#next one
     div $a0, $t0
-    mflo $a0            # $a0 = n / 10
-    
-    # Continue loop
+    mflo $a0    #mflo take /
+# Continue loop
     j sum_loop
 
 sum_done:
-    # Return sum
     jr $ra
 
-main:
-    # Print prompt
-    li $v0, 4
-    la $a0, prompt
-    syscall
-    
-    # Read integer
-    li $v0, 5
-    syscall
-    move $s0, $v0       # Store input in $s0
-    
-    # Call sumOfDigits
-    move $a0, $s0       # Set argument
-    jal sumOfDigits     # Call function
-    
-    # Print result
-    move $a0, $v0       # Move result to $a0 for printing
-    li $v0, 1           # Print integer
-    syscall
-    
-    # Print newline
-    li $v0, 4
-    la $a0, newline
-    syscall
-    
-    # Exit program
-    li $v0, 10
-    syscall
