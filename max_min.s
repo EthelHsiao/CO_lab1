c1924b4e2cc28c4a85af91d7b502f9974eba38b5
EthelHsiao <ethelhsiao0616@gmail.com>
2025-03-19 15:08:43 +0800

    fix exception5

--- max_min.s
+++ max_min.s
@@ -4,7 +4,7 @@
 prompt: .asciiz "Enter five positive integers: "
 space: .asciiz " "
 newline: .asciiz "\n"
-array: .space 20        # 5 integers * 4 bytes = 20 bytes
+array: .word 0:5         # 初始化5個整數陣列，每個值為0
 
 .text
 .globl main
@@ -16,108 +16,117 @@ findMaxMin:
     # $a2 = address of max
     # $a3 = address of min
     
-    # Initialize max and min with first element
+    # 檢查陣列大小
+    beqz $a1, empty_array    # 如果大小為0，跳轉
+    
+    # 初始化max和min為第一個元素
     lw $t0, 0($a0)      # $t0 = arr[0]
     sw $t0, 0($a2)      # *max = arr[0]
     sw $t0, 0($a3)      # *min = arr[0]
     
-    # Initialize loop counter
+    # 初始化迴圈計數器
     li $t1, 1           # i = 1
     
 loop:
-    # Check loop condition
+    # 檢查迴圈條件
     bge $t1, $a1, loop_done    # if (i >= size) goto loop_done
     
-    # Calculate address of arr[i]
+    # 計算arr[i]的地址
     sll $t2, $t1, 2     # $t2 = i * 4
     add $t2, $a0, $t2   # $t2 = &arr[i]
     lw $t3, 0($t2)      # $t3 = arr[i]
     
-    # Load current max and min
+    # 載入當前max和min
     lw $t4, 0($a2)      # $t4 = *max
     lw $t5, 0($a3)      # $t5 = *min
     
-    # Check if arr[i] > max
+    # 檢查arr[i] > max
     ble $t3, $t4, check_min    # if (arr[i] <= *max) goto check_min
     sw $t3, 0($a2)      # *max = arr[i]
     
 check_min:
-    # Check if arr[i] < min
+    # 檢查arr[i] < min
     bge $t3, $t5, loop_continue    # if (arr[i] >= *min) goto loop_continue
     sw $t3, 0($a3)      # *min = arr[i]
     
 loop_continue:
-    # Increment loop counter
+    # 增加迴圈計數器
     addi $t1, $t1, 1    # i++
-    j loop              # go back to loop
+    j loop              # 回到迴圈開始
+    
+empty_array:
+    # 如果陣列為空，設置默認值
+    li $t0, 0
+    sw $t0, 0($a2)      # *max = 0
+    sw $t0, 0($a3)      # *min = 0
     
 loop_done:
-    jr $ra              # Return to caller
+    jr $ra              # 返回調用者
 
 main:
-    # Print prompt
+    # 分配最大值和最小值的內存空間
+    addi $sp, $sp, -8   # 在堆疊上分配8位元組
+    move $s2, $sp       # $s2 = max的地址
+    addi $s3, $sp, 4    # $s3 = min的地址
+    
+    # 打印提示
     li $v0, 4
     la $a0, prompt
     syscall
     
-    # Read five integers
-    la $s0, array       # $s0 = address of array
-    li $s1, 5           # $s1 = size of array
-    li $s2, 0           # $s2 = counter
+    # 讀取五個整數
+    la $s0, array       # $s0 = 陣列地址
+    li $s1, 5           # $s1 = 陣列大小
+    li $s4, 0           # $s4 = 計數器
     
 read_loop:
-    # Check if we've read all numbers
-    beq $s2, $s1, read_done
+    # 檢查是否已讀取所有數字
+    beq $s4, $s1, read_done
     
-    # Read integer
+    # 讀取整數
     li $v0, 5
     syscall
     
-    # Store in array
-    sll $t0, $s2, 2     # $t0 = counter * 4
-    add $t0, $s0, $t0   # $t0 = &array[counter]
-    sw $v0, 0($t0)      # array[counter] = input
+    # 存入陣列
+    sll $t0, $s4, 2     # $t0 = 計數器 * 4
+    add $t0, $s0, $t0   # $t0 = &array[計數器]
+    sw $v0, 0($t0)      # array[計數器] = 輸入值
     
-    # Increment counter
-    addi $s2, $s2, 1
+    # 增加計數器
+    addi $s4, $s4, 1
     j read_loop
     
 read_done:
-    # Allocate space for max and min
-    addi $sp, $sp, -8   # Allocate 8 bytes on stack
-    move $s2, $sp       # $s2 = address of max
-    addi $s3, $sp, 4    # $s3 = address of min
-    
-    # Call findMaxMin
-    move $a0, $s0       # $a0 = array address
-    move $a1, $s1       # $a1 = size
-    move $a2, $s2       # $a2 = address of max
-    move $a3, $s3       # $a3 = address of min
+    # 調用findMaxMin
+    move $a0, $s0       # $a0 = 陣列地址
+    move $a1, $s1       # $a1 = 大小
+    move $a2, $s2       # $a2 = max的地址
+    move $a3, $s3       # $a3 = min的地址
     jal findMaxMin
     
-    # Print max
+    # 打印max
     lw $a0, 0($s2)      # $a0 = max
-    li $v0, 1           # Print integer
+    li $v0, 1           # 打印整數
     syscall
     
-    # Print space
+    # 打印空格
     li $v0, 4
     la $a0, space
     syscall
     
-    # Print min
+    # 打印min
     lw $a0, 0($s3)      # $a0 = min
-    li $v0, 1           # Print integer
+    li $v0, 1           # 打印整數
     syscall
     
-    # Print newline
+    # 打印換行
     li $v0, 4
     la $a0, newline
     syscall
     
-    # Deallocate stack space
+    # 釋放堆疊空間
     addi $sp, $sp, 8
     
-    # Exit program
+    # 退出程式
     li $v0, 10
     syscall
